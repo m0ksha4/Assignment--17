dcf50a20a2451155313ad4a728ace4b497c28b9b
m0ksha4 <mohamedebrahim9879@gmail.com>
2026-04-27 01:35:56 +0300

    fix ignored folders

--- dist/common/utils/otp.utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateOTP = generateOTP;
-function generateOTP() {
-    const minNumber = 1000000;
-    const maxNumber = 9000000;
-    return Math.floor(Math.random() * minNumber + maxNumber);
-}
